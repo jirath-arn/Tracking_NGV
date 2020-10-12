3d3e3f787b7b0843addf096a3f6aec987344c8c8
ice191913 <60824415+ice191913@users.noreply.github.com>
2020-10-12 22:55:46 +0700

    Add search.blade.php

--- routes/web.php
+++ routes/web.php
@@ -14,13 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return redirect('/th');
+    return redirect('/search');
 });
 
-Route::get('/th', function(){
-    return view('welcome');
-});
-
-Route::get('/en', function(){
-    return view('welcome');
+Route::get('/search', function(){
+    return view('search2');
 });
