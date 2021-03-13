284e07167ef08467618469671f627230ec4f43e0
Jirath Arnamnath <ice191913@gmail.com>
2021-03-14 04:28:15 +0700

    Remove white box testing

--- app/Http/Controllers/Admin/BusController.php
+++ app/Http/Controllers/Admin/BusController.php
@@ -15,9 +15,9 @@ class BusController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function index()
-    { echo "18TC "; 
-        echo "19TC "; $buses = Bus::all();
-        echo "20TC "; return view('admin.buses.index', compact('buses'));
+    {
+        $buses = Bus::all();
+        return view('admin.buses.index', compact('buses'));
     }
 
     /**
@@ -27,9 +27,9 @@ class BusController extends Controller
      */
 
     public function create()
-    { echo "30TC "; 
-        echo "31TC "; $routes = Route::all()->pluck('name_route', 'id')->prepend(trans('global.pleaseSelect'), '');
-        echo "32TC "; return view('admin.buses.create', compact('routes'));
+    {
+        $routes = Route::all()->pluck('name_route', 'id')->prepend(trans('global.pleaseSelect'), '');
+        return view('admin.buses.create', compact('routes'));
     }
 
     /**
@@ -39,20 +39,20 @@ class BusController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
-    { echo "42TC "; 
-        echo "43TC "; $request->validate([
+    {
+        $request->validate([
             'route_id' => 'required',
             'license_plate' => 'required|max:10|unique:buses',
-        ]); echo "46TC "; 
-        echo "47TC "; 
-        echo "48TC "; $route = Route::find($request->route_id);
-        echo "49TC "; $route->buses()->create([
+        ]);
+        
+        $route = Route::find($request->route_id);
+        $route->buses()->create([
             'license_plate' => $request->license_plate,
             'latitude' => 14.0695183,
             'longitude' => 100.6032949,
-        ]); echo "53TC "; 
-        echo "54TC "; 
-        echo "55TC "; return redirect()->route('admin.buses.index');
+        ]);
+        
+        return redirect()->route('admin.buses.index');
     }
 
     /**
@@ -62,10 +62,10 @@ class BusController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function edit(Bus $bus)
-    { echo "65TC "; 
-        echo "66TC "; $routes = Route::all()->pluck('name_route', 'id')->prepend(trans('global.pleaseSelect'), '');
-        echo "67TC "; $bus->load('route');
-        echo "68TC "; return view('admin.buses.edit', compact('routes', 'bus'));
+    {
+        $routes = Route::all()->pluck('name_route', 'id')->prepend(trans('global.pleaseSelect'), '');
+        $bus->load('route');
+        return view('admin.buses.edit', compact('routes', 'bus'));
     }
 
     /**
@@ -76,17 +76,17 @@ class BusController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
-    { echo "79TC "; 
-        echo "80TC "; $request->validate([
+    {
+        $request->validate([
             'route_id' => 'required',
             'license_plate' => 'required|max:10',
-        ]); echo "83TC "; 
-        echo "84TC "; 
-        echo "85TC "; $bus = Bus::find($id);
-        echo "86TC "; $bus->route_id = $request->route_id;
-        echo "87TC "; $bus->license_plate = $request->license_plate;
-        echo "88TC "; $bus->save();
-        echo "89TC "; return redirect()->route('admin.buses.index');
+        ]);
+        
+        $bus = Bus::find($id);
+        $bus->route_id = $request->route_id;
+        $bus->license_plate = $request->license_plate;
+        $bus->save();
+        return redirect()->route('admin.buses.index');
     }
 
     /**
@@ -96,8 +96,8 @@ class BusController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function destroy(Bus $bus)
-    { echo "99TC ";
-        echo "100TC "; $bus->delete();
-        echo "101TC "; return redirect()->route('admin.buses.index');
+    {
+        $bus->delete();
+        return redirect()->route('admin.buses.index');
     }
 }
